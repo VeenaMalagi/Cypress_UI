5709e567377cda9d9d8f5c886449e54359079c4b
veena malagi <koni.veena@gmail.com>
2021-09-15 14:20:09 +0530

    path changed for cucumber html report in cucumber-html-report.js file

--- cypress/cucumber-html-report.js
+++ cypress/cucumber-html-report.js
@@ -3,7 +3,7 @@ var reporter = require('cucumber-html-reporter');
 var options = {
         theme: 'bootstrap',
         jsonFile: 'cypress/cucumber-json',
-        output: './cucumber_reports/cucumber_report.html',
+        output: 'cypress/cucumber_reports/cucumber_report.html',
         reportSuiteAsScenarios: true,
         scenarioTimestamp: true,
         launchReport: true,
